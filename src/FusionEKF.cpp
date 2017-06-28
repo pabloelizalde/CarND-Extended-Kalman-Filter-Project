cdde4f1103376f7ac6795df50bc822b5b04c93ed
Pablo Elizalde <pelizcar@gmail.com>
2017-06-28 14:23:44 +0200

    chore: Initialise values with first measurement
    Convert polar coordinates to cartesian coordinates for radar measurement

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -59,29 +59,25 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Initialization
    ****************************************************************************/
   if (!is_initialized_) {
-    /**
-    TODO:
-      * Initialize the state ekf_.x_ with the first measurement.
-      * Create the covariance matrix.
-      * Remember: you'll need to convert radar from polar to cartesian coordinates.
-    */
-    // first measurement
-    cout << "EKF: " << endl;
+
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      /**
-      Convert radar from polar to cartesian coordinates and initialize state.
-      */
+      double cosval = cos(measurement_pack.raw_measurements_[1]);
+      double sinval = sin(measurement_pack.raw_measurements_[1]);
+      ekf_.x_ << measurement_pack.raw_measurements_[0] * cosval, measurement_pack.raw_measurements_[0] * sinval, 0, 0;
+      Hj_ = tools.CalculateJacobian(ekf_.x_);
+      ekf_.H_ = Hj_;
+      ekf_.R_ = R_radar_;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-      Initialize state.
-      */
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+      ekf_.H_ = H_laser_;
+      ekf_.R_ = R_laser_;
     }
 
-    // done initializing, no need to predict or update
+    previous_timestamp_ = measurement_pack.timestamp_;
     is_initialized_ = true;
     return;
   }
